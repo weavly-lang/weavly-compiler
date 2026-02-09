373dddabb8645c010356ab5d323df49581d19aff
Felix Groß <felix-gross@mail.de>
2026-02-09 11:24:41 +0100

    fix multiple new line bug in env file

--- src/weavly/parsing/wenvl_transformer.py
+++ src/weavly/parsing/wenvl_transformer.py
@@ -1,27 +1,37 @@
 from typing import Any
 
-from lark import Transformer, v_args
+from lark import Discard, Transformer, v_args
 
 
 @v_args(inline=True)
 class WenvlTransformer(Transformer):
+    DEFAULT_STRING = ""
+    DEFAULT_NUMBER = 0.0
+    DEFAULT_FLAG = False
+
     def start(self, *children) -> dict[str, list]:
         return {"declarations": list(children)}
 
-    def string_declaration(self, name: str, value: str | None) -> dict[str, str]:
-        if value == None:
-            value = ""
-        return {"type": "string", "name": name, "value": value}
+    def string_declaration(self, name: str, value=None) -> dict[str, str]:
+        if value is None:
+            value = self.DEFAULT_STRING
+        return {"type": "string", "name": name, "value": str(value)}
     
-    def number_declaration(self, name: str, min: float | None, max: float | None,  value: float | None) -> dict[str, str]:
-        if value == None:
-            value = 0.0
-        return {"type": "number", "name": name, "value": value, "min": min, "max": max}
+    def number_declaration(self, name: str, min=None, max=None, value=None) -> dict[str, Any]:
+        if value is None:
+            value = self.DEFAULT_NUMBER
+        return {"type": "number", "name": name, "value": float(value), "min": min, "max": max}
     
-    def flag_declaration(self, name: str, value: bool | None) -> dict[str, str]:
-        if value == None:
-            value = False
-        return {"type": "flag", "name": name, "value": value}
+    def flag_declaration(self, name: str, value=None) -> dict[str, Any]:
+        if value is None:
+            value = self.DEFAULT_FLAG
+        return {"type": "flag", "name": name, "value": bool(value)}
+
+    def blank_line(self) -> Any:
+        return Discard
+
+    def ID(self, token):
+        return str(token)
 
     def STRING(self, token):
         return token[1:-1]
