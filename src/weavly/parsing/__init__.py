30c2b1c7c2e13ef4ad98deeb77ae2a8ed49ee98a
Felix Groß <104097884+grossf@users.noreply.github.com>
2026-07-07 16:02:15 +0200

    Remove the weavly watch command (#9)

--- src/weavly/parsing/__init__.py
+++ src/weavly/parsing/__init__.py
@@ -1,9 +1,7 @@
-from .file_handler import FileHandler
 from .parser import WVL_BUILD_EXTENSION, WVL_SOURCE_EXTENSION, build_all_files
 
 __all__ = [
     "build_all_files",
     "WVL_SOURCE_EXTENSION",
     "WVL_BUILD_EXTENSION",
-    "FileHandler",
 ]
